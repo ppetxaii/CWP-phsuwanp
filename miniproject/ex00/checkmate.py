c6245c8b583d9c55f4875173bb652f7d5d324195
ppetxaii <68050396@kmitl.ac.th>
2026-09-25 12:57:06 +0700

    eiei

--- miniproject/ex00/checkmate.py
+++ miniproject/ex00/checkmate.py
@@ -67,7 +67,7 @@ def checkmate(board):
                 for dr, dc in directions:
                     rr, cc = r + dr, c + dc
                     while 0 <= rr < n and 0 <= cc < n:
-                        if grid[rr][cc] not in ('.', ' '):
+                        if grid[rr][cc] in ('P', 'B', 'R', 'Q', 'K'):
                             if (rr, cc) == king_pos:
                                 is_check = True
                             break
